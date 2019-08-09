f1f8ca2495115843d4ba0e40763db00b2c85a06e
dafyddllyr <dafyddllyr@gmail.com>
2019-08-09 12:57:16 +0100

    POI created in db

--- src/main/java/com/codeclan/example/backend/components/DataLoader.java
+++ src/main/java/com/codeclan/example/backend/components/DataLoader.java
@@ -2,7 +2,10 @@ package com.codeclan.example.backend.components;
 
 import com.codeclan.example.backend.models.locations.Accommodation;
 import com.codeclan.example.backend.models.locations.AccommodationType;
+import com.codeclan.example.backend.models.locations.PointOfInterest;
+import com.codeclan.example.backend.models.locations.PointOfInterestType;
 import com.codeclan.example.backend.repositories.AccommodationRepository;
+import com.codeclan.example.backend.repositories.PointOfInterestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -16,6 +19,9 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     AccommodationRepository accommodationRepository;
 
+    @Autowired
+    PointOfInterestRepository pointOfInterestRepository;
+
     public DataLoader() {
     }
 
@@ -25,8 +31,13 @@ public class DataLoader implements ApplicationRunner {
         coords1.add(-10.00);
         coords1.add(10.00);
         Accommodation accom1 = new Accommodation("BillyBob's Hotel", 5, "Nice!!", coords1, AccommodationType.HOTEL);
-
         accommodationRepository.save(accom1);
+
+        ArrayList<Double> coords2 = new ArrayList<>();
+        coords2.add(-20.00);
+        coords2.add(20.00);
+        PointOfInterest poi1 = new PointOfInterest("Milngavie", 3, "Small town with all the shops you need", coords2, PointOfInterestType.TOWN);
+        pointOfInterestRepository.save(poi1);
 
     }
 
---
+++ src/main/java/com/codeclan/example/backend/controllers/PointOfInterestController.java
@@ -0,0 +1,9 @@
+package com.codeclan.example.backend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class PointOfInterestController {
+}
