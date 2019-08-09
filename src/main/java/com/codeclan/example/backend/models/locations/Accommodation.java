7f51beb2eb04810822a7f9e92145816dc95b5b93
StewartMacLeman <stewart_macleman@hotmail.com>
2019-08-09 10:21:27 +0100

    accomodation inhertied stuff

--- src/main/java/com/codeclan/example/backend/models/locations/Accommodation.java
+++ src/main/java/com/codeclan/example/backend/models/locations/Accommodation.java
@@ -1,4 +1,14 @@
 package com.codeclan.example.backend.models.locations;
 
+import java.util.ArrayList;
+
 public class Accommodation extends Location {
+
+    public Accommodation(String name, int rating, String description, ArrayList<Double> coordinates) {
+        super(name, rating, description, coordinates);
+    }
+
+    public Accommodation() {
+    }
+
 }
--- src/main/java/com/codeclan/example/backend/models/locations/AccommodationType.java
+++ src/main/java/com/codeclan/example/backend/models/locations/AccommodationType.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.backend.models.locations;
 
 public enum AccommodationType {
+
+    HOTEL,
+    B_AND_B,
+    CAMPSITES,
+    HOSTEL
 }
